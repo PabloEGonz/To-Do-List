d7986ef64755d2635892eff30fa3c3703e2df614
Pablo <ezekielgonzalezr@gmail.com>
2023-05-04 14:34:06 -0600

    Create funtionabilities

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const listContainer = document.querySelector('.task-container');
 
 const displayList = () => {
   listContainer.innerHTML = '';
-  tasks.forEach((task) => {
+  tasks.forEach((task, index) => {
+    task.index = index;
     const list = document.createElement('li');
     list.classList.add('list');
     list.innerHTML = ` <img class='checkbox' src='${checkBox}'> <p class='task-description'>${task.description}</p> <img class='options' src='${dots}'> `;
@@ -19,19 +20,21 @@ const displayList = () => {
   });
 };
 displayList();
+
 const submitBtn = document.querySelector('#submitBtn');
 const addInput = document.querySelector('.add');
-//const addTask = ()=>
+let localTasks = localStorage.getItem('tasks');
 const getLocalStorage = () => {
-  let localTasks = localStorage.getItem('tasks');
   if (localTasks) {
     tasks = JSON.parse(localTasks);
     displayList();
-
   }
 }
 getLocalStorage();
-console.log(tasks)
+
+const setLocalStorage = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   let newTask = {
@@ -40,7 +43,7 @@ submitBtn.addEventListener('click', (e) => {
     index: tasks.length
   }
   tasks.push(newTask);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  setLocalStorage();
   displayList();
   check();
 })
@@ -48,26 +51,67 @@ submitBtn.addEventListener('click', (e) => {
 const optionBtn = document.getElementsByClassName('options');
 const taskDescription = document.getElementsByClassName('task-description');
 const listElem = document.getElementsByClassName('list');
-
-
 const checked = document.getElementsByClassName('checkbox');
+
 const check = () => {
   for (let i = 0; i < optionBtn.length; i++) {
-    optionBtn[i].addEventListener('click', () => {
-      listElem[i].classList.add('edit')
-      listElem[i].innerHTML = `<img class='checkbox' src='${checkBox}'> <input class="add" type="text" value=${taskDescription[i].textContent} > <img class='options' src='${trashIcon}'>`
-      
+    taskDescription[i].addEventListener('dblclick', () => {
+      removeSingle();
+      taskDescription[i].innerHTML = `<input class="add" id="edit-input" type="text" value="${tasks[i].description}"></input>`
+      optionBtn[i].src = trashIcon;
+      listElem[i].classList.add('edit');
+      let editInput = document.querySelector('#edit-input');
+      editInput.focus();
+      editInput.addEventListener('blur', () => {
+        tasks[i].description = editInput.value;
+        setTimeout(update, 222);
+      });
     })
   }
+  const removeSingle = () => {
+    for (let i = 0; i < optionBtn.length; i++) {
+      optionBtn[i].addEventListener('click', () => {
+        if (optionBtn[i].src !== dots) {
+          tasks.splice(i, 1);
+          update();
+        }
+      })
+    }
+  }
+
 
   for (let i = 0; i < checked.length; i++) {
     checked[i].addEventListener('click', () => {
-      checked[i].src = completeIcon;
+      if (tasks[i].completed === false) {
+        taskDescription[i].classList.add('crossed');
+        checked[i].src = completeIcon;
+        tasks[i].completed = true;
+      } else {
+        taskDescription[i].classList.remove('crossed');
+        checked[i].src = checkBox;
+        tasks[i].completed = false;
+      }
     })
   }
 }
 check();
 
+const update = () => {
+  displayList();
+  setLocalStorage();
+  check();
+};
 
-
+const removeBtn = document.querySelector('.remove');
+const deleteAll = () => {
+  removeBtn.addEventListener('click', () => {
+    for (let i = 0; i < checked.length; i++) {
+      if (tasks[i].completed) {
+        tasks.splice(i, 1);
+      }
+      update();
+    }
+  })
+}
+deleteAll();
 
