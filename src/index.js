4cb94d2d8dd277d1f730debb60834d3a76d68be0
Pablo <ezekielgonzalezr@gmail.com>
2023-05-05 13:35:07 -0600

    Import fumctions  from modules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import './style.css';
 // eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies
 import './images/enter.jpg';
+import './images/reload.png'
 
 import {
-  displayList, addList, deleteAll, editTask,
+  displayList, addList, deleteAll, editTask, reload
 } from './modules/displayList.js';
 
 displayList();
 addList();
 editTask();
 deleteAll();
+reload();
+
+
